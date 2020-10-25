c2209d6449ec2f81e5c6727c8e2dcd3e4414a204
FujieW <Wfj050160458>
2020-10-25 22:03:01 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ SwordOffer/src/main/java/com/fujiew/MaxDepth.java
@@ -0,0 +1,20 @@
+package com.fujiew;
+
+import com.fujiew.common.TreeNode;
+
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2020/10/25 22:00
+ **/
+public class MaxDepth {
+    public static void main(String[] args) {
+
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 1;
+        }
+        return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
+    }
+}
