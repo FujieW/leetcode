66a5ef2b52ec654cdf305f264f89da149251661f
FujieW <Wfj050160458>
2020-10-30 08:24:20 +0800

    二叉树结点

---
+++ EveryDay/src/main/java/common/TreeNode.java
@@ -0,0 +1,18 @@
+package common;
+
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2020/10/29 18:55
+ **/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.left = null;
+        this.right = null;
+        val = x;
+    }
+}
