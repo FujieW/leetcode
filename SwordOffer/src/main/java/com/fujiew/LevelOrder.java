984440767080a2fd8c4b33c7941a52c321f7e8c1
FujieW <Wfj050160458>
2020-11-05 10:53:26 +0800

    剑指 Offer 32 - I. 从上到下打印二叉树

--- SwordOffer/src/main/java/com/fujiew/LevelOrder.java
+++ SwordOffer/src/main/java/com/fujiew/LevelOrder.java
@@ -2,9 +2,7 @@ package com.fujiew;
 
 import com.fujiew.common.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author fujiew
@@ -13,21 +11,40 @@ import java.util.List;
  **/
 public class LevelOrder {
     public static void main(String[] args) {
-        TreeNode three = new TreeNode(3);
-        TreeNode nine = new TreeNode(9);
-        TreeNode twenty = new TreeNode(20);
-        TreeNode fifteen = new TreeNode(15);
-        TreeNode seven = new TreeNode(7);
-        three.left = nine;
-        three.right = twenty;
-        twenty.left = fifteen;
-        twenty.right = seven;
-        System.out.println(levelOrder(three));
-        System.out.println(levelOrder(null));
 
     }
 
-    public static List<List<Integer>> levelOrder(TreeNode root) {
+    /**
+     * 层次遍历第一题
+     *
+     * @param root
+     * @return
+     */
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[]{};
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode pop = queue.pop();
+            result.add(pop.val);
+            if (pop.left != null) {
+                queue.add(pop.left);
+            }
+            if (pop.right != null) {
+                queue.add(pop.right);
+            }
+        }
+        int[] array = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            array[i] = result.get(i);
+        }
+        return array;
+
+    }
+    public static List<List<Integer>> levelOrder2(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
         }
@@ -47,13 +64,11 @@ public class LevelOrder {
                     break;
                 }
                 level++;
-                // System.out.println();
                 result = new ArrayList<>();
                 nodes.add(null);
                 continue;
             }
             result.add(node.val);
-            // System.out.println(node.val + " ");
             if (node.left != null) {
                 nodes.add(node.left);
             }
@@ -62,6 +77,42 @@ public class LevelOrder {
             }
         }
 
+        return results;
+    }
+    public static List<List<Integer>> levelOrder3(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        LinkedList<TreeNode> nodes = new LinkedList<>();
+        List<List<Integer>> results = new LinkedList<>();
+        LinkedList<Integer> result = new LinkedList<>();
+        nodes.add(root);
+        nodes.add(null);
+        int level = 1;
+        while (!nodes.isEmpty()) {
+            TreeNode firstNode = nodes.pop();
+            if (firstNode == null) {
+                results.add(result);
+                if (nodes.isEmpty()) {
+                    break;
+                }
+                level++;
+                result = new LinkedList<>();
+                nodes.add(null);
+                continue;
+            }
+            if ((level & 1) != 0) {
+                result.addLast(firstNode.val);
+            } else {
+                result.addFirst(firstNode.val);
+            }
+            if (firstNode.left != null) {
+                nodes.add(firstNode.left);
+            }
+            if (firstNode.right != null) {
+                nodes.add(firstNode.right);
+            }
+        }
         return results;
     }
 }
