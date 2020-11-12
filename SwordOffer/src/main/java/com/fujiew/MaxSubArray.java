36eef7d6ecd6824cffca620987b2408a85c4030d
FujieW <136274906@qq.com>
2020-11-12 17:31:03 +0800

    最大子序列和，增加动态规划二维数组

--- SwordOffer/src/main/java/com/fujiew/MaxSubArray.java
+++ SwordOffer/src/main/java/com/fujiew/MaxSubArray.java
@@ -7,8 +7,8 @@ package com.fujiew;
  **/
 public class MaxSubArray {
     public static void main(String[] args) {
-        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, 5, 4}));
-        System.out.println(maxSubArray(new int[]{-1}));
+        System.out.println(maxSubArray2(new int[]{-2, 1, -3, 4, -1, 2, 1, 5, 4}));
+        System.out.println(maxSubArray2(new int[]{-1}));
 
     }
 
@@ -25,5 +25,18 @@ public class MaxSubArray {
             }
         }
         return max;
+    }
+
+    public static int maxSubArray2(int[] nums) {
+        int[][] dp = new int[nums.length][2];
+        int len = nums.length;
+        dp[0][0] = Integer.MIN_VALUE;
+        dp[0][1] = nums[0];
+
+        for (int i = 1; i < len; i ++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = Math.max(dp[i-1][1] + nums[i], nums[i]);
+        }
+        return Math.max(dp[len-1][0], dp[len-1][1]);
     }
 }
