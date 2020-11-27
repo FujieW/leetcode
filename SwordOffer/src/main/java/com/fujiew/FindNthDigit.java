db60b8253693b17925af8fbdf60b3425547005af
FujieW <136274906@qq.com>
2020-11-27 15:55:45 +0800

    未知题号

---
+++ SwordOffer/src/main/java/com/fujiew/FindNthDigit.java
@@ -0,0 +1,17 @@
+package com.fujiew;
+
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2020/11/12 17:36
+ **/
+public class FindNthDigit {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Character.isDigit('e'));
+    }
+
+    public int findNthDigit(int n) {
+        return 0;
+    }
+}
---
+++ SwordOffer/src/main/java/com/fujiew/MinNumber.java
@@ -0,0 +1,13 @@
+package com.fujiew;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2020/11/12 23:49
+ **/
+public class MinNumber {
+
+}
