600fad3dbc89135e8472f111601c576e22af8629
FujieW <136274906@qq.com>
2020-12-08 22:52:42 +0800

    538. 把二叉搜索树转换为累加树

---
+++ Tree/src/main/java/com/fujie/ConvertBST.java
@@ -0,0 +1,24 @@
+package com.fujie;
+
+import com.fujie.common.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2020/12/8 22:21
+ **/
+public class ConvertBST {
+    int sum = 0;
+    public TreeNode  convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
