8ab7e311b2c15a4f33a7ded7d2135ce44228fd30
FujieW <136274906@qq.com>
2021-01-18 21:01:50 +0800

    剑指 Offer 47. 礼物的最大价值

---
+++ SwordOffer/src/main/java/com/fujiew/MaxValue.java
@@ -0,0 +1,35 @@
+package com.fujiew;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2021/1/18 20:35
+ **/
+public class MaxValue {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxValue(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                } else if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                } else {
+                    grid[i][j] += Math.max(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
