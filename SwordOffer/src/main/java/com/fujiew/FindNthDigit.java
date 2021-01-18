10765e86a542a8884342aca2d9e3987cdcf4f6ef
FujieW <136274906@qq.com>
2021-01-18 23:25:11 +0800

    剑指 Offer 44. 数字序列中某一位的数字

--- SwordOffer/src/main/java/com/fujiew/FindNthDigit.java
+++ SwordOffer/src/main/java/com/fujiew/FindNthDigit.java
@@ -13,16 +13,15 @@ public class FindNthDigit {
 
     public int findNthDigit(int n) {
         int digit = 1;
-        int count = 9;
-        int start = 1;
-        while (count < n) {
+        long count = 9;
+        long start = 1;
+        while (n > count) { // 1.
             n -= count;
             digit += 1;
             start *= 10;
             count = digit * start * 9;
         }
-
-        long num = start + (n - 1) / digit;
+        long num = start + (n - 1) / digit; // 2.
         return Long.toString(num).charAt((n - 1) % digit) - '0';
     }
 }
