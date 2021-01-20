85c30a6d258c23560f10286a4bf22339a32eeca6
FujieW <136274906@qq.com>
2021-01-20 23:17:42 +0800

    剪绳子动态规划解法

--- SwordOffer/src/main/java/com/fujiew/CuttingRope.java
+++ SwordOffer/src/main/java/com/fujiew/CuttingRope.java
@@ -7,7 +7,7 @@ package com.fujiew;
  **/
 public class CuttingRope {
     public static void main(String[] args) {
-        System.out.println(cuttingRope(127));
+        System.out.println(cuttingRope2(10));
     }
 
     public static int cuttingRope(int n) {
@@ -19,5 +19,29 @@ public class CuttingRope {
             res %= 1000000007;
         }
         return (int) (res * n % 1000000007);
+    }
+
+    public static int cuttingRope2(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        int max = 0;
+        for (int i = 4; i <= n; i++) {
+            max = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                int temp = dp[j] * dp[i - j];
+                if (temp > max) {
+                    max = temp;
+                }
+                dp[i] = max;
+            }
+        }
+        max = dp[n];
+        return max;
     }
 }
