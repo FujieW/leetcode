cd6aa7ccdffd668d77d7318532e49b7aecc29c2b
FujieW <136274906@qq.com>
2021-01-25 23:29:56 +0800

    剑指 Offer 17. 打印数字，增加字符串解法

--- SwordOffer/src/main/java/com/fujiew/PrintNumbers.java
+++ SwordOffer/src/main/java/com/fujiew/PrintNumbers.java
@@ -1,5 +1,7 @@
 package com.fujiew;
 
+import java.util.Arrays;
+
 /**
  * @author fujiew
  * @TODO : TODO
@@ -7,7 +9,7 @@ package com.fujiew;
  **/
 public class PrintNumbers {
     public static void main(String[] args) {
-
+        printNumbers2(2);
     }
 
     public int[] printNumbers(int n) {
@@ -17,5 +19,32 @@ public class PrintNumbers {
             result[i] = i + 1;
         }
         return result;
+    }
+
+    public static void printNumbers2(int n) {
+        if (n < 0) {
+            return;
+        }
+        char[] nums = new char[n + 1];
+        for (int i = 0; i < 10; i++) {
+            char c = (char) (i + '0');
+            nums[0] = c;
+            printNums(nums, n, 0);
+
+        }
+
+    }
+
+    private static void printNums(char[] nums, int n, int index) {
+        if (index == n - 1) {
+            String str = new String(nums);
+            System.out.println(str);
+            return;
+        }
+        for (int i = 0; i < 10; i++) {
+            char c = (char) (i + '0');
+            nums[index + 1] = c;
+            printNums(nums, n, index + 1);
+        }
     }
 }
