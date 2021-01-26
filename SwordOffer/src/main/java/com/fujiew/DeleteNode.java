87cdb1c50969d1f6665534dac1f87569704b4638
FujieW <136274906@qq.com>
2021-01-26 09:31:24 +0800

    剑指 Offer 18. 删除链表的节点

--- SwordOffer/src/main/java/com/fujiew/DeleteNode.java
+++ SwordOffer/src/main/java/com/fujiew/DeleteNode.java
@@ -14,7 +14,7 @@ public class DeleteNode {
         ListNode head = new ListNode(-3);
         head.next = new ListNode(5);
         head.next.next = new ListNode(-99);
-        ListNode node = deleteNode(head, 5);
+        ListNode node = deleteNode2(head, -99);
         while (node != null) {
             System.out.println(node.val);
             node = node.next;
@@ -33,6 +33,29 @@ public class DeleteNode {
             it = it.next;
         }
         it.next = it.next.next;
+        return head;
+    }
+
+    public static ListNode deleteNode2(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        ListNode curr = head;
+        for (int i = 0; curr != null; i++) {
+            if (i == 0) {
+                if (curr.val == val) {
+                    return curr.next;
+                }
+            } else {
+                prev = prev.next;
+            }
+            curr = curr.next;
+            if (curr.val == val) {
+                prev.next = curr.next;
+                break;
+            }
+        }
         return head;
     }
 }
