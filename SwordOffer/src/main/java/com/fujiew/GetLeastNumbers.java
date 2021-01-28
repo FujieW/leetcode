8f1c8669e4a664ee15639a6b72b9fc5f5fd564bb
FujieW <136274906@qq.com>
2021-01-28 14:45:30 +0800

    获取最小K个数，新增快速排序解法

--- SwordOffer/src/main/java/com/fujiew/GetLeastNumbers.java
+++ SwordOffer/src/main/java/com/fujiew/GetLeastNumbers.java
@@ -9,7 +9,9 @@ import java.util.Arrays;
  **/
 public class GetLeastNumbers {
     public static void main(String[] args) {
-
+        GetLeastNumbers getLeastNumbers = new GetLeastNumbers();
+        int[] nums = getLeastNumbers.getLeastNumbers2(new int[]{0,0,1,2,4,2,2,3,1,4}, 8);
+        System.out.println(Arrays.toString(nums));
     }
 
     public int[] getLeastNumbers(int[] arr, int k) {
@@ -21,5 +23,46 @@ public class GetLeastNumbers {
             i++;
         }
         return result;
+    }
+
+    public int[] getLeastNumbers2(int[] arr, int k) {
+        if (arr.length <= k) {
+            return arr;
+        }
+        if (k <= 0) {
+            return new int[]{};
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (true) {
+            int index = help(arr, k, low, high);
+            if (index == k - 1) {
+                int[] newArr = new int[k];
+                for (int i = 0; i < k; i++) {
+                    newArr[i] = arr[i];
+                }
+                return newArr;
+            } else if (k <= index) {
+                high = index - 1;
+            } else {
+                low = index + 1;
+            }
+        }
+    }
+
+    private int help(int[] arr, int k, int low, int high) {
+        int pivot = arr[low];
+        while (low < high) {
+            while (low < high && arr[high] >= pivot) {
+                high--;
+            }
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= pivot) {
+                low++;
+            }
+            arr[high] = arr[low];
+        }
+        arr[low] = pivot;
+        return low;
     }
 }
