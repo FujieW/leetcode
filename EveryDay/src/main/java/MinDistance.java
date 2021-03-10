ec9cd7df20b6e607c2cff7142870d840dc23050e
FujieW <136274906@qq.com>
2021-03-10 16:02:02 +0800

    编辑距离 备忘录递归解法

--- EveryDay/src/main/java/MinDistance.java
+++ EveryDay/src/main/java/MinDistance.java
@@ -6,9 +6,38 @@
 public class MinDistance {
     public static void main(String[] args) {
         MinDistance minDistance = new MinDistance();
-        System.out.println(minDistance.minDistance("rad", "apple"));
+        String word1 = "rad";
+        String word2 = "apple";
+        // System.out.println(minDistance.minDistance(word1, word2));
+        System.out.println(minDistance.minDistance(word1, word2));
+
     }
+
     public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()][word2.length()];
+        return minDistance(word1, word2, word1.length() - 1, word2.length() - 1, dp);
+    }
+
+    public int minDistance(String word1, String word2, int n, int m, int[][] dp) {
+
+        if (n == -1) {
+            return m + 1;
+        }
+        if (m == -1) {
+            return n + 1;
+        }
+        if (dp[n][m] != 0) {
+            return dp[n][m];
+        }
+        if (word1.charAt(n) == word2.charAt(m)) {
+            dp[n][m] = minDistance(word1, word2, n - 1, m - 1, dp);
+        } else {
+            dp[n][m] = Math.min(minDistance(word1, word2, n - 1, m - 1, dp) + 1,
+                    Math.min(minDistance(word1, word2, n - 1, m, dp) + 1, minDistance(word1, word2, n, m - 1, dp) + 1));
+        }
+        return dp[n][m];
+    }
+    /*public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
@@ -31,5 +60,5 @@ public class MinDistance {
             }
         }
         return dp[m][n];
-    }
+    }*/
 }
