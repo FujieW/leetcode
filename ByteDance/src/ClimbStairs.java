e29c20eb340a1b0c923e38af65809ecc498b8942
FujieW <136274906@qq.com>
2021-04-07 10:25:55 +0800

    爬楼梯

---
+++ ByteDance/src/ClimbStairs.java
@@ -0,0 +1,27 @@
+/**
+ * @author fujiew
+ * @TODO : TODO
+ * @Create on : 2021/4/7 10:20
+ **/
+public class ClimbStairs {
+    public static void main(String[] args) {
+
+    }
+
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
